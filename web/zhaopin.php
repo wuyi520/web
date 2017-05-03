c92105217fa2707148461d590b4774410f37fb9a
945346899 <945346899@qq.com>
2017-05-03 19:58:48 +0800

    ceshi

---
+++ web/zhaopin.php
@@ -0,0 +1,173 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>招聘人才</title>
+	<style>
+		#head{width: 100%;height: 107px;border-bottom: 1px solid #000;}
+		
+		#content{width: 1000px;min-height: 500px;
+			margin: 0px auto;overflow: hidden;}
+		#footer{width: 100%;height: 128px;background: #0fa9eb;}
+		#head #head_box #logo{width: 485px;height: 107px;float: left;}
+		#head  #head_box{width: 1000px;height: 107px;margin: 0px auto;}
+		#head #head_box #search{width: 400px;height: 107px;float: right;text-align: right;line-height: 15px;}
+		#head #head_box #search p a{font-size: 14px;color: #333;text-decoration: none;}
+		 #fom{width: 400px;height: 50px;}
+		 #sub{background: #0fa9eb;border:1px solid #0fa9eb;float: right;width: 60px;height: 36px;color: #fff;}
+		 #txt{border :1px solid #0fa9eb;float: right;width: 210px;height: 32px;}
+		#title{width: 100%;height: 340px;}
+		#title #topnav{width: 1000px;height: 50px;margin: 0px auto;}
+		#title #img{width: 100%;height: 300px;background: #6495ed;}
+		#content #left{width: 190px;min-height: 500px;float: left;}
+		#content #right{width: 780px;min-height: 500px;float: right;}
+		#title #topnav .tp{width: 142.5px;height: 50px;font-size: 18px;display: block;	
+			line-height: 50px;text-decoration: none;text-align: center;float: left;
+			color: #000;}
+		#title #topnav :hover{background:#0fa9eb;color: #fff ;}
+		#content #left #left_tit{width: 190px;height: 60px;background: #6fcbf3;color: #fff;}
+		#content #left #left_span{width: 100px;height: 50px;font-size: 24px;background: #6fcbf3;
+						color :#fff;line-height: 60px;text-align: center;margin-left: 20px;
+						border-bottom: 1px #fff solid;}
+		#content #left #left_link_1{width: 190px;height: 36px;background: #0fa9eb;font-size:16px; 
+						text-decoration: none;line-height: 36px;text-indent: 20px;
+						display: block;color: #fff ;}	
+		#content #left #left_link_2{width: 190px;height: 36px;font-size: 16px;text-decoration: none;
+						line-height: 36px;text-indent: 20px;display: block;background: #eee;}
+		#content #left #left_link_2:hover{background: #0fa9eb;color:#fff;}					
+		#content #right #right_tit{width: 72px;height:44px; color:#fff;
+					border-bottom: 3px solid #10a9ec;line-height: 60px;font-size:18px;color:#0fa9eb;float: left;}
+		#content #right #right_line{width: 708px;height: 45px;
+					border-bottom:1px solid #000;float: left;}	
+		#content #right .links{width: 780px;height: 50px;border-bottom:1px dashed #000 ;overflow: hidden;}
+		#content #right .links .jop{width: 150px;height: 50px;text-decoration: none;line-height: 50px;display: block;
+					float: left;font-size: 16px;color: #000;}	
+		#content #right .links .jop:hover{color: #10a9ec;}				
+		#content #right .links .application{width: 150px;height: 50px;text-decoration: none;line-height: 40px;display: block;
+					float: right;text-align: right;font-size: 16px;color: #0fa9eb;}
+			
+		#page{width: 200px;height: 70px;margin: 0px auto;margin-top: 30px;}	
+		#page #page_1{width: 60px;height: 30px;font-size: 14px;background: #f3f3f3;
+				border-bottom: 2px solid #f3f3f3;float:left; line-height: 30px;text-align: center;margin-top: 20px;}
+		#page #page_2{height: 30px;width: 20px;background:#0fa9eb;color: #fff;font-size:14px;float: left;
+				line-height:30px;text-align: center;margin: 10px; margin-top: 20px;}
+		#page #page_3{width: 60px;height: 30px;font-size: 14px;background: #f3f3f3;border-bottom: 2px solid #f3f3f3;float: left;
+				line-height: 30px;text-align: center;margin-top: 20px;}	
+		#footer #footer_world{width: 1000px;height: 100px;margin:0px auto;padding-top: 10px;}
+		#footer #footer_world p{font-size: 14px;color:#fff;text-align: center;}
+		#footer #footer_world p a{text-decoration: none ;color: #fff ;}
+	</style>
+</head>
+<body>
+	<div id="head"> 
+			<div id="head_box">
+				<div id="logo"><a href=""><img src="./images/logo.png"></a></div>
+				<div id="search">
+							<p>
+								<a href="">设为首页</a> |
+								<a href="">加入收藏</a>
+							</p>
+							<p>
+								<form id="fom" action="" method="post">
+								<input id="sub" type="submit" name="sub" value="搜索">
+								<input id="txt" placeholder="请输入关键词......" type="text">
+							</p>
+
+				</div>
+			</div>
+	</div>
+	
+				<div id="title">
+					<div id="topnav"><a class="tp" href="">网站首页</a>;
+				<a class="tp" href="">关于诚技</a>';
+				<a class="tp" href="">新闻资讯</a>';
+				<a class="tp" href="">产品中心</a>';
+				<a class="tp" href="">成功案例</a>';
+				<a class="tp" href="">人才招聘</a>';
+				<a class="tp" href="">联系我们</a>';
+	
+	
+	
+				</div>
+	 
+				<div id="img"><img src="./images/dalian5.jpg"></div>';
+
+	</div>
+	<div id="content">
+				<div id="left">
+						<div id="left_tit"> 
+							<div id="left_span">人才招聘</div>
+						</div>';
+				<a id="left_link_1" href="">>招聘职位</a>';
+				<a id="left_link_2"  href"="">>在线留言</a>';
+
+				</div>
+				<div id="right"> 
+						<div id="right_tit">招聘职位</div>
+						<div id="right_line"></div>';
+	
+
+				<div class="links">
+								<a class="jop" href="">机械工程师</a>
+								<a class="application" href="">应聘职位</a>
+						</div>';
+				<div class="links">
+								<a class="jop" href="">机械设计主管</a>
+								<a class="application" href="">应聘职位</a>
+						</div>';
+				<div class="links">
+								<a class="jop" href="">电气自动化工程师</a>
+								<a class="application" href="">应聘职位</a>
+						</div>';
+				<div class="links">
+								<a class="jop" href="">电气自动化设计主管</a>
+								<a class="application" href="">应聘职位</a>
+						</div>';
+				<div class="links">
+								<a class="jop" href="">液压工程师</a>
+								<a class="application" href="">应聘职位</a>
+						</div>';
+				<div class="links">
+								<a class="jop" href="">液压钳工</a>
+								<a class="application" href="">应聘职位</a>
+						</div>';
+				<div class="links">
+								<a class="jop" href="">招聘电工</a>
+								<a class="application" href="">应聘职位</a>
+						</div>';
+				<div class="links">
+								<a class="jop" href="">装配钳工</a>
+								<a class="application" href="">应聘职位</a>
+						</div>';
+	?>
+						<div id="page">
+							<div id="page_1">上一页</div>
+							<div id="page_2">1</div>
+							<div id="page_3">下一页</div>
+						</div>
+					
+			</div>
+
+	</div>
+	<div id="footer">
+			<div id="footer_world">	
+				<p>
+				<a href="">关于诚技 </a>|
+				<a href=""> 新闻资讯</a> |
+				<a href="">  产品中心</a> | 
+				<a href=""> 成功案例</a> | 
+				<a href="">人才招聘</a>  |
+				<a href=""> 联系我们</a> </p>
+				<p>Copyright © 大连诚技机电设备有限公司 版权所有 辽ICP备13015234号-2</p>
+				<p>网站设计：<a href="">仟亿科技</a></P>
+			</div>
+
+
+
+
+
+
+	</div>
+</body>
+</html>
